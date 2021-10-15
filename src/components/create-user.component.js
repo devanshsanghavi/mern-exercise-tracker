ce4739c97aa7ccd49b04a6a686e37e546ffd30ea
unknown <devansh.sanghavi@somaiya.edu>
2021-10-15 13:44:23 +0530

    Connected CreateExercise and CreateUser to backend

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class CreateUser extends Component {
 
@@ -26,6 +26,9 @@ export default class CreateUser extends Component {
             username: this.state.username
         }
         console.log(user);
+
+        axios.post('http://localhost:5000/users/add', user)
+        .then(res => console.log(res.data));
         this.setState({
             username: ''
         });
