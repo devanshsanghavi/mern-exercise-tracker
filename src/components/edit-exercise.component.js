d7cc84c844a98634855c488709077b18fd08555e
unknown <devansh.sanghavi@somaiya.edu>
2021-10-15 13:22:11 +0530

    Updated App.js, Added components

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class EditExercise extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on the Edit Exercise Component</p>
+      </div>
+    );
+  }
+}
